a6d4f650e30ff63dcfd47914bb5840921b969dd6
Kelson Hartle <kelson99hartle@icloud.com>
2020-08-10 14:45:54 -0600

    Multiple choice quiz now working:

--- Quizzler-iOS13/Question.swift
+++ Quizzler-iOS13/Question.swift
@@ -10,11 +10,13 @@ import Foundation
 
 struct Question {
     var text: String
-    var answer: String
+    var answersArray: [String]
+    var correctAnswer: String
     
-    init(q: String, a: String) {
+    init(q: String, a: [String], correctAnswer: String) {
         self.text = q
-        self.answer = a
+        self.answersArray = a
+        self.correctAnswer = correctAnswer
     }
 }
 
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -16,20 +16,27 @@ enum TrueFalse : String {
 class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
-        
+    @IBOutlet weak var optionNumberOne: UIButton!
+    @IBOutlet weak var optionNumberTwo: UIButton!
+    @IBOutlet weak var optionNumberThree: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     var quizController = QuizController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        scoreLabel.text = "Score: 0"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         updateUI()
     }
     
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         guard let userAnswer = sender.currentTitle else { return }// TRUE FALSE
+        
         let userCorrectAnswer = quizController.checkAnswer(userAnswer)
         
         if userCorrectAnswer {
@@ -45,14 +52,24 @@ class ViewController: UIViewController {
     }
     
     @objc func updateUI() {
+        
+//        guard let optionOne = self.quizController.answerOne, let optionTwo = self.quizController.answerTwo, let optionThree = self.quizController.answerThree else { return }
 
-        questionLabel.text = quizController.getQuestionText()
-        trueButton.backgroundColor = .clear
-        falseButton.backgroundColor = .clear
-        progressBar.progress = quizController.getProgress()
+        DispatchQueue.main.async {
+            self.optionNumberOne.setTitle(self.quizController.answerOne, for: .normal)
+            self.optionNumberTwo.setTitle(self.quizController.answerTwo, for: .normal)
+            self.optionNumberThree.setTitle(self.quizController.answerThree, for: .normal)
+        }
+            
+            self.questionLabel.text = self.quizController.getQuestionText()
+            self.scoreLabel.text = "Score: \(self.quizController.getScore())"
+            self.optionNumberOne.backgroundColor = .clear
+            self.optionNumberTwo.backgroundColor = .clear
+            self.optionNumberThree.backgroundColor = .clear
+            self.progressBar.progress = self.quizController.getProgress()
+        
     }
 }
-
 
 extension ViewController {
     private func createAlert(message: String, title: String) {
